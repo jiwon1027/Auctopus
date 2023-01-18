3d1a1930b880982fb322e157a89127bf98c0451f
yezgoget <yejison@gmail.com>
2023-01-18 11:04:17 +0900

    ✨ feat: hmerge header-layout-footer

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -5,14 +5,13 @@ import {
   BellIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
-import { ReactComponent as Turtle } from "../../assets/badges/turtle.svg";
 
 function Header() {
   return (
     <div className={styles.header}>
       {/* <ChevronLeftIcon className="h-6 w-6 " onClick={() => navigate(-1)} /> */}
-      <Turtle className="w-14 h-10" />
-      <div className="w-14 flex flex-row justify-between">
+      <h1>Auctopus</h1>
+      <div className={styles.leftBox}>
         <BellIcon className="h-6 w-6 " />
         <MagnifyingGlassIcon className="h-6 w-6" />
       </div>
--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -2,18 +2,14 @@ import React from "react";
 import styles from "./Root.module.css";
 import ItemsList from "../components/common/ItemsList";
 import Header from "../components/common/Header";
-import i from "@components/common/";
-
 import Footer from "../components/common/Footer";
 export default function Root() {
   return (
     <>
-      <Header />
       <div className={styles.root}>
-        <div className="header bg-turtle-standard h-14"> Header</div>
-        <div className="content">
-          <ItemsList />
-        </div>
+        <Header />
+        <ItemsList />
+        <div className="content"></div>
         <Footer />
       </div>
     </>
