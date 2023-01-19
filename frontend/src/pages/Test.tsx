7a6098cb26540944c4db502f4a34156cae35e028
김태원 <taw4654@gmail.com>
2023-01-19 13:31:18 +0900

    ✨ feat: add Material-UI and Material-icons

---
+++ frontend/src/pages/Test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+// import Slider from "@mui/material";
+import Button from "@mui/material/Button";
+import { styled as mstyled } from "@mui/material/styles";
+import Slider from "@mui/material/Slider";
+import styled from "styled-components";
+
+const CustomizedSlider = mstyled(Slider)`
+  color: #20b2aa;
+
+  :hover {
+    /* color: #2e8b57; */
+    color: red
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+function Test() {
+  //   return <CustomizedSlider defaultValue={30} />;
+  return (
+    <div>
+      <Title>Hello, world :)</Title>
+      {/* <Slider defaultValue={30} /> */}
+      hello test
+      <Button variant="contained">Hello World</Button>
+      <CustomizedSlider defaultValue={30} />
+    </div>
+  );
+}
+
+export default Test;
