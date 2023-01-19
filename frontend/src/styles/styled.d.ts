22124310ea14c2007e8a42fdf0de334f5ac30d11
yezgoget <yejison@gmail.com>
2023-01-19 13:42:45 +0900

    ✨ feat: add global style

---
+++ frontend/src/styles/styled.d.ts
@@ -0,0 +1,28 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      turtleDark: string;
+      turtleStandard: string;
+      turtleLight: string;
+      greyDark: string;
+      greyStandard: string;
+      greyLight: string;
+      greyDim: string;
+    };
+    fontWeight: {
+      hairline: number;
+      extraLight: number;
+      thin: number;
+      light: number;
+      normal: number;
+      medium: number;
+      semibold: number;
+      bold: number;
+      extrabold: number;
+      extraBold: number;
+      black: number;
+    };
+  }
+}
---
+++ frontend/src/styles/theme.tsx
@@ -0,0 +1,26 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    turtleDark: "#386641",
+    turtleStandard: "#6A994E", // bg-turtle-standard
+    turtleLight: "#A7C957",
+    greyDark: "#5C5B5B",
+    greyStandard: "#8E8E8E",
+    greyLight: "#E6E6E6",
+    greyDim: "#D1D1D1",
+  },
+  fontWeight: {
+    hairline: 100,
+    extraLight: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    extraBold: 800,
+    black: 900,
+  },
+};
