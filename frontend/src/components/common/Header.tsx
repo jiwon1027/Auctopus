d0283e1321e80bd701b39cbd9138ccd19c4de0b8
김태원 <taw4654@gmail.com>
2023-01-19 17:51:03 +0900

    ✨ feat: add Navigation using react-router

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,16 +1,42 @@
 import React from "react";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ChevronLeftOutlinedIcon from "@mui/icons-material/ChevronLeftOutlined";
 import styled from "styled-components";
 import { theme } from "@/styles/theme";
 import { ReactComponent as Turtle } from "../../assets/badges/turtle.svg";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export default function Header() {
+interface IProps {
+  title?: string;
+  rightComponent?: JSX.Element;
+}
+
+/**
+ * 헤더
+ *
+ * @param props.title 제목
+ * @param props.rightComponent 오른쪽 코너 컴포넌트
+ * @returns Header component
+ */
+export default function Header(props: IProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname; // || "/"
   return (
     <StyledHeader>
-      {/* <ChevronLeftIcon className="h-6 w-6 " onClick={() => navigate(-1)} /> */}
-      <Turtle width="5rem" height="4rem" />
+      {from ? (
+        <ChevronLeftOutlinedIcon
+          className="h-6 w-6 "
+          onClick={() => navigate(-1)}
+        />
+      ) : (
+        <Turtle width="5rem" height="4rem" />
+      )}
+      {/* TODO: add a title from props */}
       <div className="iconContainer">
+        {/* FIXME: be moved to main page */}
         <NotificationsNoneOutlinedIcon className="icon" />
         <SearchOutlinedIcon className="icon" />
       </div>
