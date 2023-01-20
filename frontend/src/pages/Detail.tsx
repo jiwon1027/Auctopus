6b71201643ff04d133db86b5e264adf165488de5
yezgoget <yejison@gmail.com>
2023-01-20 13:15:57 +0900

    ✨ feat: detail layout complete

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import DummyImg from "@/assets/detail/dummy.svg";
-// import { HeartIcon } from "@heroicons/react/24/outline";
 import styled from "styled-components";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { styled as mstyled } from "@mui/material/styles";
 import { theme } from "@/styles/theme";
-
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 export default function Detail() {
   return (
     <>
       <Container>
         <ImgBox>
+          <Link to={"/"}>
+            <CustomizeIcon />
+          </Link>
+
           <img src={DummyImg} alt="dummy-img" />
         </ImgBox>
 
@@ -34,14 +41,46 @@ export default function Detail() {
           <div className="contentCataegory">패션 잡화</div>
           <div className="contentDescription">
             정품맞고 이탈리아 현지 구매했습니다. 가로37 세로25cm 정도 (손잡이
-            미포함) 개런티카드,더스트백, 행텍 풀구성 외관 기스 없이 깨끗한편
+            미포함)
+            <br />
+            <br />
+            개런티카드,더스트백, 행텍 풀구성 외관 기스 없이 깨끗한편
           </div>
         </ContentBox>
-        <FooterBox></FooterBox>
+        <FooterBox>
+          <div className="timeBox">
+            <div className="timephrase">
+              <AccessTimeIcon color="disabled" sx={{ fontSize: 20 }} />
+              <span>경매시작까지</span>
+            </div>
+            <div className="timeLeft">1일 01: 14: 50</div>
+          </div>
+          <div className="buttonBox">
+            <CustomizedButton>경매시작</CustomizedButton>
+          </div>
+        </FooterBox>
       </Container>
     </>
   );
 }
+
+const CustomizeIcon = mstyled(ArrowBackIosIcon)`
+  width: 3rem;
+  height: 3rem;
+  color: white;
+  position: absolute;
+  margin-top: 1.5rem;
+  margin-left: 1.9rem;
+`;
+const CustomizedButton = mstyled(Button)`
+  border: solid 2px ${theme.colors.turtleDark};
+  font-size: 1.8rem;
+  font-weight: ${theme.fontWeight.bold};
+  color: ${theme.colors.turtleDark};
+  width: 12.1rem;
+  height: 4.8rem;
+  border-radius: 10;
+`;
 
 //imgBox 35 profileBox 10 ContentBox
 const Container = styled.div`
@@ -67,6 +106,7 @@ const ProfileBox = styled.div`
   height: 10%;
   padding: 0.5rem;
   display: flex;
+  border-bottom: 1px solid ${theme.colors.greyLight};
   .profileIconBox {
     width: 20%;
     display: flex;
@@ -77,16 +117,16 @@ const ProfileBox = styled.div`
     display: flex;
     width: 60%;
     flex-direction: column;
-    padding: 1.6em;
+    padding: 1.4rem;
   }
   .infoTitle {
-    font-size: 1.92rem;
+    font-size: 1.6rem;
     height: 40%;
     font-weight: ${theme.fontWeight.bold};
   }
   .infoBadge {
     height: 20%;
-    margin-top: 1.16rem;
+    margin-top: 1rem;
     font-size: 1.2rem;
     color: ${theme.colors.turtleStandard};
   }
@@ -103,25 +143,49 @@ const ContentBox = styled.div`
   padding: 2.4rem;
   display: flex;
   height: 35%;
-  border: solid 2px green;
   flex-direction: column;
   .contentTitle {
-    font-size: 1.92rem;
-    height: 20%;
-    border: solid 2px red;
+    font-size: 2.2rem;
+    font-weight: ${theme.fontWeight.bold};
   }
   .contentCataegory {
-    height: 15%;
-    font-size: 0.8rem;
-    color: gray;
-    border: solid 2px red;
+    margin-top: 1rem;
+    font-size: 1.2rem;
+    color: ${theme.colors.greyStandard};
   }
   .contentDescription {
-    height: 70%;
-    border: solid 2px red;
+    margin-top: 1.7rem;
+    font-size: 1.5rem;
   }
 `;
 
 const FooterBox = styled.div`
   height: 15%;
+  border-top: 1px solid ${theme.colors.greyLight};
+  padding: 2.5rem 2.7rem;
+  display: flex;
+  .timeBox {
+    width: 60%;
+    span {
+      font-size: 1.2rem;
+      margin-left: 0.6rem;
+      margin-top: 0.2rem;
+      color: ${theme.colors.greyStandard};
+    }
+    .timephrase {
+      display: flex;
+      align-items: center;
+    }
+    font-size: 2.5rem;
+    font-weight: ${theme.fontWeight.bold};
+    .timeLeft {
+      margin-top: 1rem;
+    }
+  }
+  .buttonBox {
+    width: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
