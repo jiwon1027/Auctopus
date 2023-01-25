e0b64738f97defd0df9141e76d134d81bcfed18a
leejiwon <won64312000@naver.com>
2023-01-25 12:32:09 +0900

    ⛏  fix: fix repository

--- backend/src/main/java/com/auctopus/project/db/repository/AuctionImageRepository.java
+++ backend/src/main/java/com/auctopus/project/db/repository/AuctionImageRepository.java
@@ -4,17 +4,20 @@ import com.auctopus.project.db.domain.AuctionImage;
 import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuctionImageRepository {
+@EnableJpaRepositories
+public interface AuctionImageRepository extends JpaRepository<AuctionImage, Integer> {
 
-    AuctionImage findById(Long imageId);
+    AuctionImage findByAuctionImageSeq(Long auctionImageSeq);
 
-    Optional<List<AuctionImage>> findByAuctionSeq(Long auction_seq);
+    Optional<List<AuctionImage>> findByAuctionSeq(Long auctionSeq);
 
     @Transactional
     @Modifying(clearAutomatically = true)
-    void deleteAllByAuctionSeq(Long auction_seq);
+    void deleteAllByAuctionSeq(Long auctionSeq);
 }
--- backend/src/main/java/com/auctopus/project/db/repository/AuctionRepository.java
+++ backend/src/main/java/com/auctopus/project/db/repository/AuctionRepository.java
@@ -3,9 +3,11 @@ package com.auctopus.project.db.repository;
 import com.auctopus.project.db.domain.Auction;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
 
 }
