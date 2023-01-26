c90aec847561f67106b5c1c0e0920ac8d11f2db9
yezgoget <yejison@gmail.com>
2023-01-26 14:32:13 +0900

    ⛏ fix: header layout

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,29 +1,13 @@
 import React from "react";
 import ItemsList from "../components/common/ItemsList";
-// import Footer from "../components/common/Footer";
-import styled from "styled-components";
-import Header from "@components/common/Header";
+import Layout from "@components/common/Layout";
 
 export default function Root() {
   return (
     <>
-      <Container>
-        <Header leftIcon={"none"} />
+      <Layout leftIcon="turtle">
         <ItemsList />
-        {/* <Footer /> */}
-      </Container>
+      </Layout>
     </>
   );
 }
-
-const Container = styled.div`
-  background-color: white;
-  margin-left: auto;
-  margin-right: auto;
-  width: 390px;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  position: relative;
-`;
