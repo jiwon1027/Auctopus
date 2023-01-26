abfcce3e12f4b78e77508a64829853f52f3851cb
yezgoget <yejison@gmail.com>
2023-01-26 14:04:06 +0900

    ✨ feat: change hamburger layout

--- frontend/src/components/common/Hamburger.tsx
+++ frontend/src/components/common/Hamburger.tsx
@@ -1,21 +1,31 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import ProfileImg from "@/assets/common/profile.png";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+import AssignmentIndOutlinedIcon from "@mui/icons-material/AssignmentIndOutlined";
+import ExitToAppOutlinedIcon from "@mui/icons-material/ExitToAppOutlined";
 import { Link } from "react-router-dom";
-
 import MenuIcon from "@mui/icons-material/Menu";
-import HomeIcon from "@mui/icons-material/Home";
-import TextsmsIcon from "@mui/icons-material/Textsms";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import styled from "styled-components";
+import { styled as mstyled } from "@mui/material/styles";
+import { theme } from "@/styles/theme";
 type Anchor = "top" | "left" | "bottom" | "right";
 
+const navItems = [
+  { value: "메인 화면", uri: "/" },
+  { value: "채팅", uri: "/chat" },
+  { value: "찜 목록", uri: "/likes" },
+  { value: "내 프로필", uri: "/profile" },
+];
 export default function Hamburger() {
   const [state, setState] = React.useState({
     top: false,
@@ -39,40 +49,56 @@ export default function Hamburger() {
     };
 
   const list = (anchor: Anchor) => (
-    <Box
-      sx={{ width: 250 }}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {["Home", "Chat", "Likes", "Profile"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton
-              key={index}
-              component={Link}
-              to={`/${text.toLowerCase()}`}
-            >
-              <ListItemIcon key={index}>
-                {index % 2 != 0 ? (
-                  text === "Chat" ? (
-                    <TextsmsIcon />
+    <>
+      <CustomClose onClick={toggleDrawer(anchor, false)} />
+      {/* <Logo src={LogoImg} alt="logo" /> */}
+      <ProfileBox>
+        <Link to={`/profile`}>
+          {" "}
+          <Profile src={ProfileImg} alt="profile" />
+        </Link>
+
+        <span className="profileTitle">정개미님</span>
+      </ProfileBox>
+
+      <Box
+        sx={{ width: 300 }}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+      >
+        <Divider />
+        <List>
+          {navItems.map((obj, index) => (
+            <ListItem key={obj.value} disablePadding>
+              <ListItemButton key={index} component={Link} to={obj.uri}>
+                <ListItemIcon key={index}>
+                  {index % 2 != 0 ? (
+                    obj.value === "채팅" ? (
+                      <CustomText />
+                    ) : (
+                      <CustomAccount />
+                    )
+                  ) : obj.value == "메인 화면" ? (
+                    <CustomHome />
                   ) : (
-                    <AccountBoxIcon />
-                  )
-                ) : text == "Home" ? (
-                  <HomeIcon />
-                ) : (
-                  <FavoriteBorderIcon />
-                )}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-    </Box>
+                    <CustomFavorite />
+                  )}
+                </ListItemIcon>
+                <CustomListText>{obj.value}</CustomListText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+          <Divider />
+          <ListItemButton>
+            <ListItemIcon>
+              <CustomLogout />.
+            </ListItemIcon>
+            <CustomListTextL>로그 아웃</CustomListTextL>
+          </ListItemButton>
+        </List>
+      </Box>
+    </>
   );
 
   return (
@@ -92,3 +118,75 @@ export default function Hamburger() {
     </div>
   );
 }
+
+const ProfileBox = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .profileTitle {
+    font-size: 1.8rem;
+    margin: 1.2rem 0;
+    font-family: Pretendard;
+    text-align: center;
+    font-weight: ${theme.fontWeight.semibold};
+  }
+`;
+const Profile = styled.img`
+  width: 6rem;
+  cursor: pointer;
+`;
+
+const CustomClose = mstyled(CloseOutlinedIcon)`
+widtH: 2.5rem;
+height: 2.5rem;
+display: flex;
+margin-left: auto;
+padding: 1.5rem;
+`;
+
+const CustomHome = mstyled(HomeOutlinedIcon)`
+width: 2.5rem;
+height: 2.5rem;
+  color: ${theme.colors.turtleDark};
+  padding: 1rem;
+  `;
+const CustomText = mstyled(ChatOutlinedIcon)`
+width: 2.5rem;
+height: 2.5rem;
+  color: ${theme.colors.turtleDark};
+  padding: 1rem;
+`;
+const CustomFavorite = mstyled(FavoriteBorderIcon)`
+width: 2.5rem;
+height: 2.5rem;
+  color: ${theme.colors.turtleDark};
+  padding: 1rem;
+`;
+const CustomAccount = mstyled(AssignmentIndOutlinedIcon)`
+width: 2.5rem;
+height: 2.5rem;
+  color: ${theme.colors.turtleDark};
+  padding: 1rem;
+`;
+
+const CustomListText = styled.div`
+  font-size: 1.8rem;
+  font-weight: ${theme.fontWeight.extraBold};
+  color: ${theme.colors.turtleDark};
+  font-family: Pretendard;
+`;
+const CustomListTextL = styled.div`
+  font-size: 1.8rem;
+  font-weight: ${theme.fontWeight.extraBold};
+  color: ${theme.colors.greyStandard};
+  font-family: Pretendard;
+`;
+
+const CustomLogout = mstyled(ExitToAppOutlinedIcon)`
+width: 2.5rem;
+height: 2.5rem;
+  color: ${theme.colors.greyStandard};
+  padding: 1rem;
+`;
