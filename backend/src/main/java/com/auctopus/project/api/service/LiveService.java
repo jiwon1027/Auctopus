825a081327433442f6fb0efc597f53b1e98b8bdf
Sangbin <qlsqlsz1@naver.com>
2023-01-30 17:39:51 +0900

    ✨  feat add live service

--- backend/src/main/java/com/auctopus/project/api/service/LiveService.java
+++ backend/src/main/java/com/auctopus/project/api/service/LiveService.java
@@ -4,13 +4,15 @@ import com.auctopus.project.db.domain.Live;
 
 public interface LiveService {
 
+    void creataLive(int auctionSeq, String userEmail);
+
     Live getLive(int liveSeq);
 
     void increaseViewer(int liveSeq);
 
     void decreaseViewer(int liveSeq);
 
-//    void increaseParticipant(int liveSeq);
-//
-//    void increaseParticipant(int liveSeq);
+    void increaseParticipant(int liveSeq);
+
+    void decreaseParticipant(int liveSeq);
 }
