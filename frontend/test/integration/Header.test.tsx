7ec905fda99b6fed940d9056905aae4f9399619c
Dan Kim <dankim0213@gmail.com>
2023-01-31 08:05:56 +0900

    🧪 test: add a test suit for signup

--- frontend/test/integration/Header.test.tsx
+++ frontend/test/integration/Header.test.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Header from "@components/common/Header";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
+import { BrowserRouter as Router } from "react-router-dom";
 import { theme } from "@/styles/theme";
+import { ThemeProvider } from "styled-components";
 
 describe("simple test", () => {
   it("tests that title is properly shown", () => {
     render(
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Header title="this title" />
-        </BrowserRouter>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Header title="this title" leftIcon="none" />
+        </ThemeProvider>
+      </Router>
     );
     expect(screen.getByText(/This title/i)).toBeDefined();
   });
