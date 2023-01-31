a3947d9ac74b1316b89dff1dc6007aabe1fc3f47
leejiwon <won64312000@naver.com>
2023-01-31 13:30:27 +0900

    ⛏  fix: fix cors config & login code

--- backend/src/main/java/com/auctopus/project/api/controller/KakaoUserController.java
+++ backend/src/main/java/com/auctopus/project/api/controller/KakaoUserController.java
@@ -11,6 +11,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +34,7 @@ public class KakaoUserController {
     private UserServiceImpl userServiceImpl;
 
     @GetMapping("/login")
+    @CrossOrigin("*")
     public ResponseEntity<HashMap<String, Object>> kakaoLogin(@RequestParam String code) {
         HashMap<String, Object> resultMap = new HashMap<>();
         HttpStatus status = null;
@@ -55,7 +57,7 @@ public class KakaoUserController {
 
             // email이 현재 DB에 저장있지 않으면 DB에 저장
             if (userRepository.findByEmail((String) kakaoUserInfo.get("email")).isEmpty()) {
-                resultMap.put("username", null);
+                resultMap.put("nickname", null);
                 resultMap.put("userEmail", null);
                 resultMap.put("newUser", 0);
 
@@ -66,7 +68,7 @@ public class KakaoUserController {
 
             }
             else{
-                resultMap.put("username", kakaoUserInfo.get("nickname"));
+                resultMap.put("nickname", kakaoUserInfo.get("nickname"));
                 resultMap.put("userEmail", kakaoUserInfo.get("email"));
                 resultMap.put("newUser", 1);
             }
@@ -89,6 +91,9 @@ public class KakaoUserController {
 
         System.out.println("신규 유저 정보 입력");
 
+        HashMap<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+
         try {
             System.out.println(user.toString());
 
@@ -104,11 +109,19 @@ public class KakaoUserController {
                             .nickname((String) authentication.getCredentials())
                     .build());
 
+            resultMap.put("userName", authentication.getPrincipal());
+            resultMap.put("userEmail", authentication.getCredentials());
+            status = HttpStatus.OK;
+            resultMap.put(("message"), "회원 저장");
+
 
         } catch (Exception e) {
             System.out.println("에러발생@@@@@@@@@@@@@@@@@");
+            resultMap.put(("message"), e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+
         }
 
-        return null;
+        return new ResponseEntity<HashMap<String, Object>>(resultMap, status);
     }
 }
