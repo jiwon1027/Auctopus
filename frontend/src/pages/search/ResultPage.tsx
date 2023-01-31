a2d7b46451964c922545b434d84f052aeda0f9e3
정상기 <jackid1103@naver.com>
2023-01-31 17:15:24 +0900

    ✨ feat: add Category search router

--- frontend/src/pages/search/ResultPage.tsx
+++ frontend/src/pages/search/ResultPage.tsx
@@ -8,39 +8,35 @@ import Image2 from "@/assets/main/airpodsImg.jpg";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
-const liveAuction: IAuction[] = [
-  {
-    img: Image1,
-    title: "검색결과",
-    price: 450000,
-    viewer: 55,
-    time: "2023-01-27 12:00",
-  },
-  {
-    img: Image2,
-    title: "검색결과2",
-    price: 410000,
-    viewer: 15,
-    time: "2023-01-27 12:05",
-  },
-];
-
 export default function ResultPage() {
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("keyword"); // test
-  const [live, setLive] = useState<"live" | "nonLive">("live");
+  const keywordQuery = searchParams.get("keyword");
+  const categoryQuery = searchParams.get("category");
 
+  const [auctionList, setAuctionList] = useState<IAuction[]>([]);
+
+  const [live, setLive] = useState<"live" | "nonLive">("live");
   const changeLive = () => {
     setLive((prev) => (prev === "live" ? "nonLive" : "live"));
   };
   return (
     <Layout leftIcon="none">
       <SearchBar />
-      <ResultText>
-        &lsquo;<b>{query}</b>&rsquo;에 대한 검색결과입니다.
-      </ResultText>
+      {keywordQuery !== null ? (
+        <ResultText>
+          &lsquo;
+          <b>{keywordQuery}</b>
+          &rsquo;에 대한 검색결과입니다.
+        </ResultText>
+      ) : (
+        <ResultText>
+          &lsquo;
+          <b>{categoryQuery}</b>
+          &rsquo;카테고리에 대한 검색결과입니다.
+        </ResultText>
+      )}
       <ResultFilter live={live} onClick={changeLive} />
-      <ItemsList liveAuction={liveAuction} isLive={live === "live"} />
+      <ItemsList liveAuction={auctionList} isLive={live === "live"} />
     </Layout>
   );
 }
