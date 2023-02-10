16a5ea3af9c7ad78ab2509ba67cfcdb9612aee06
byeonyujeong <bbnt25@naver.com>
2023-02-10 15:26:24 +0900

    ✨ feat:image like auction

--- backend/src/main/java/com/auctopus/project/api/controller/SearchController.java
+++ backend/src/main/java/com/auctopus/project/api/controller/SearchController.java
@@ -3,14 +3,12 @@ package com.auctopus.project.api.controller;
 import com.auctopus.project.api.response.AuctionListResponse;
 import com.auctopus.project.api.service.AuctionImageService;
 import com.auctopus.project.api.service.AuctionService;
-import com.auctopus.project.api.service.CategoryService;
 import com.auctopus.project.api.service.LiveService;
 import com.auctopus.project.db.domain.Auction;
 import com.auctopus.project.db.domain.AuctionImage;
 import com.auctopus.project.db.domain.Live;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,31 +36,22 @@ public class SearchController {
     @Autowired
     private LiveService liveService;
 
-    @Autowired
-    private CategoryService categoryService;
-
-
     // authentication는 최신 검색기록 때문에 필요해서 param에 넣음(보류)
     // 검색창에 경매 검색 했을 때
     @CrossOrigin("*")
     @GetMapping()
     public ResponseEntity<List<AuctionListResponse>> getAuctionListByWordOrCategory(
-            @Nullable Authentication authentication, @RequestParam(required = false) String word, @RequestParam(required=false)String category,
+            @Nullable Authentication authentication, @RequestParam(required = false) String word, @RequestParam(required=false)int category,
             @RequestParam int state) throws UnsupportedEncodingException {
         List<Auction> auctionList = null;
         int categorySeq = 0;
-        if(category != null) {
-            category = URLDecoder.decode(category,"UTF-8");
-            System.out.println(category);
-            categorySeq = categoryService.getCategorySeq(category);
-        }
+        if (category > 0) categorySeq = category;
         if (state == 1) { // 시청자 수(Live - viewer)로 sort
             auctionList = auctionService.getAuctionListByViewerAndWordOrCategorySeq(word, categorySeq, state);
         } else { // 좋아요(Auction - likeCount)로 sort
             auctionList = auctionService.getAuctionListByLikeCountAndWordOrCategorySeq(word, categorySeq, state);
         }
         List<AuctionListResponse> auctionListResponseList = new ArrayList<>();
-        System.out.println(auctionList);
         for (Auction auction : auctionList) {
 //            List<AuctionImage> auctionImageList = null;
             List<AuctionImage> auctionImageList = auctionImageService.getAuctionImageListByAuctionSeq(auction.getAuctionSeq());
@@ -80,33 +69,4 @@ public class SearchController {
         }
         return new ResponseEntity<>(auctionListResponseList, HttpStatus.OK);
     }
-
-
-    // 검색창에 카테고리 네모박스(8개 있는거) 클릭했을 때
-//    @CrossOrigin("*")
-//    @GetMapping("/category")
-//    public ResponseEntity<List<AuctionListResponse>> getAuctionListByCategory(
-//            @RequestParam("category") String category, @RequestParam("state") int state) {
-//        List<Auction> auctionList = null;
-//        int categorySeq = categoryService.getCategorySeq(category);
-//        if (state == 1) { // 시청자 수(Live - viewer)로 sort
-//            auctionList = auctionService.getAuctionListByViewerAndCategory(categorySeq, state);
-//        } else { // 좋아요(Auction - likeCount)로 sort
-//            auctionList = auctionService.getAuctionListByLikeCountAndCategory(categorySeq, state);
-//        }
-//        List<AuctionListResponse> auctionListResponseList = new ArrayList<>();
-//        for (Auction auction : auctionList) {
-//            //List<AuctionImage> auctionImageList = null;
-//            List<AuctionImage> auctionImageList = auctionImageService.getAuctionImageListByAuctionSeq(auction.getAuctionSeq());
-//            if (state == 1) {
-//                Live live = liveService.getLiveInfo(auction.getAuctionSeq());
-//                auctionListResponseList.add(AuctionListResponse.of(auction, live.getViewer(),
-//                        live.getCurrentPrice(), auctionImageList));
-//            } else {
-//                auctionListResponseList.add(AuctionListResponse.of(auction, 0,
-//                        auction.getStartPrice(), auctionImageList));
-//            }
-//        }
-//        return new ResponseEntity<>(auctionListResponseList, HttpStatus.OK);
-//    }
 }
