bcef023498b80f818562e45cd6964e745b7db8d4
김태원 <taw4654@gmail.com>
2023-02-13 11:19:18 +0900

    ⛏ fix: replace StompJS to WebSocket

--- frontend/src/pages/auction/BidPage.tsx
+++ frontend/src/pages/auction/BidPage.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-// import SockJs from "sockjs-client/dist/sockjs";
-import { Client } from "@stomp/stompjs";
 import Layout from "@components/common/Layout";
 import { Button } from "@mui/material";
 import NoticeSection from "@components/bidding/NoticeSection";
@@ -9,9 +7,7 @@ import ActionFooter from "@components/bidding/ActionFooter";
 import { IMessage } from "types/auction";
 import { useLocation } from "react-router-dom";
 import useAuth from "@/store/atoms/useAuth";
-import WebSocket from "isomorphic-ws";
-const global = globalThis;
-Object.assign(global, { WebSocket });
+import WebSocket from "ws";
 
 const initAuctionInfo: IAuctionDetail = {
   auctionSeq: 1,
@@ -33,8 +29,8 @@ const initAuctionInfo: IAuctionDetail = {
 export default function BidPage() {
   const location = useLocation();
   const user = useAuth().getUser();
+  const [webSocket, setWebSocket] = useState<WebSocket>();
   const [messages, setMessages] = useState<IMessage[]>([]);
-  const [client, setClient] = useState<Client>();
   const { auctionInfo, userState, limit } = (location.state as {
     auctionInfo: IAuctionDetail;
     userState: string;
@@ -43,89 +39,36 @@ export default function BidPage() {
   console.log("auctioninfo", auctionInfo);
 
   useEffect(() => {
-    const newClient = new Client({
-      brokerURL: "ws://localhost:8081/api/ws-stomp",
-      onConnect: () => {
-        newClient.subscribe(
-          "/bid/room/" + auctionInfo.auctionSeq,
-          (message) => {
-            console.log(`Received: ${message.body}`);
-            // const msg = JSON.parse(message.body);
-            // setMessages((prev) => [...prev, message]);
-          }
-        );
-        newClient.publish({
-          destination: "/bid/room/" + auctionInfo.auctionSeq,
-          body: "First Message",
-        });
-      },
-    });
-    newClient.activate();
-    setClient(newClient);
+    const newWebSocket = new WebSocket(
+      `${import.meta.env.VITE_WEBSOCKET_DOMAIN}/live/${auctionInfo.auctionSeq}`
+    );
 
-    return () => {
-      newClient.deactivate();
+    newWebSocket.onopen = (event) => {
+      newWebSocket.send(
+        "Here's some text that the server is urgently awaiting!"
+      );
     };
-  }, []);
 
-  // useEffect(() => {
-  //   const socket = new SockJS("http://localhost:8081/api/ws-stomp");
-  //   const newClient = Stomp.over(socket);
-  //   setClient(newClient);
-  //   newClient.debug = (...args: string[]) => {
-  //     console.log(args);
-  //   };
-  //   newClient.connect({}, () => {
-  //     newClient.subscribe(
-  //       "/sub/chat/room/" + auctionInfo.auctionSeq,
-  //       function (message: Stomp.Message) {
-  //         // FIXME: messagedto
-  //         const messagedto = JSON.parse(message.body);
-  //         console.log(
-  //           auctionInfo.auctionSeq === messagedto.roomId,
-  //           auctionInfo.auctionSeq,
-  //           messagedto.roomId
-  //         );
-  //         if (auctionInfo.auctionSeq === messagedto.roomId) {
-  //           setMessages((prev) => [...prev, messagedto]);
-  //         }
-  //       },
-  //       (err: Error) => {
-  //         console.log(err);
-  //       }
-  //     );
-  //   });
-  //   return () => {
-  //     /**
-  //      * 1. 판매자가 나간 경우, 서버에 말하고 모든 이들에게 메시지 남기고 모든 경매를 중지
-  //      * 2. 구매자가 나간 경우, 서버에 말하고 메시지 남기고 중지
-  //      */
-  //     newClient.disconnect(() => {
-  //       if (userState === "seller") {
-  //         sendMessage("E10");
-  //       } else {
-  //         sendMessage("E20");
-  //       }
-  //     });
-  //   };
-  // }, [auctionInfo.auctionSeq]);
+    newWebSocket.onmessage = (event) => {
+      setMessages((prev) => [...prev, JSON.parse(event.data as string)]);
+    };
+
+    setWebSocket(newWebSocket);
+    return () => newWebSocket.close();
+  }, []);
 
   // FIXME: type
   const sendMessage = (chat: string) => {
-    const msg: IMessage = {
-      type: 2,
-      date: "",
-      liveSeq: auctionInfo.auctionSeq,
-      message: chat,
-      nickname: user.nickname,
-      userEmail: user.email,
-    };
-    // client?.send(`/pub/chat/message`, {}, JSON.stringify(msg));
-    // client?.publish({ destination: "/pub/chat/message" });
-    client?.publish({
-      destination: "/bid/room/" + auctionInfo.auctionSeq,
-      body: JSON.stringify(msg),
-    });
+    webSocket?.send(
+      JSON.stringify({
+        type: 2,
+        date: "",
+        liveSeq: auctionInfo.auctionSeq,
+        message: chat,
+        nickname: user.nickname,
+        userEmail: user.email,
+      } as IMessage)
+    );
   };
 
   return (
