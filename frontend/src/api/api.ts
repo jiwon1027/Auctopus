db404ed63ca7a2ce751606742c10cc9bc27f69cb
김태원 <taw4654@gmail.com>
2023-02-16 11:19:58 +0900

    ⛏ fix: hotfix the redirect uri when loggin in

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const KAKAO_HOST = "https://kauth.kakao.com";
 const REST_API_KEY = import.meta.env.VITE_REST_API_KEY;
 const REDIRECT_URI = `${
-  import.meta.env.VITE_LOCAL_DOMAIN
+  import.meta.env.VITE_REDIRECT_URL
 }/oauth/callback/kakao`;
 
 export const KAKAO_AUTH_URL = `${KAKAO_HOST}/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
